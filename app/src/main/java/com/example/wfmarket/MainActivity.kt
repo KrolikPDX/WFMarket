f51482c2fd6e8e2c0f7ef4dd56996cd1bdc3dd8b
Joseph Demyanovskiy <48137744+KrolikPDX@users.noreply.github.com>
2022-12-11 19:05:20 -0800

    Added login and skip button func

--- app/src/main/java/com/example/wfmarket/MainActivity.kt
+++ app/src/main/java/com/example/wfmarket/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.wfmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.loginButton).setOnClickListener(loginButtonClicked())
+        findViewById<Button>(R.id.skipLoginButton).setOnClickListener(skipButtonClicked())
+    }
+
+    private fun loginButtonClicked(): View.OnClickListener {
+        return View.OnClickListener {
+            var text:String = findViewById<EditText>(R.id.emailTextBox).text.toString()
+            var password:String = findViewById<EditText>(R.id.passwordTextBox).text.toString()
+            //Attempt to login to warframe marketplace
+            //Log.i("Print", "Email: $text")
+        }
+    }
+
+    private fun skipButtonClicked(): View.OnClickListener {
+        return View.OnClickListener {
+            //Move on to next screen and remember option
+        }
     }
 }
