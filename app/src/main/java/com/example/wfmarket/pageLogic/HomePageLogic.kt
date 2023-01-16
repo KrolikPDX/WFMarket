b1c1eb040aade044af190e2ca3e3ad0ce393d59a
Joseph Demyanovskiy <48137744+KrolikPDX@users.noreply.github.com>
2023-01-16 15:32:35 -0800

    Replaced navigation with toolbar

--- app/src/main/java/com/example/wfmarket/pageLogic/HomePageLogic.kt
+++ app/src/main/java/com/example/wfmarket/pageLogic/HomePageLogic.kt
@@ -2,9 +2,13 @@ package com.example.wfmarket.pageLogic
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.wfmarket.R
 import com.example.wfmarket.models.responses.auth.AuthSigninResponse
@@ -16,9 +20,12 @@ import com.google.gson.Gson
 var user: User? = null
 
 class HomePageLogic: AppCompatActivity(){
+    private lateinit var toolbar: Toolbar
     private lateinit var toggle:ActionBarDrawerToggle
     private lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
+
+    private lateinit var navigationUserName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +38,22 @@ class HomePageLogic: AppCompatActivity(){
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.i("Print", "Checking if item match: $item")
         if (toggle.onOptionsItemSelected(item)) {
-            Log.i("Print", "Clicked on item!")
             return true
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun setupParams() {
+        toolbar = findViewById(R.id.navigation_toolbar)
+        setSupportActionBar(toolbar)
+
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationView)
+
+        val navigationHeader: View = navigationView.getHeaderView(0)
+        navigationUserName = navigationHeader.findViewById(R.id.navigation_user_name)
+
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
         var authSigningResponse:String?
@@ -51,16 +63,28 @@ class HomePageLogic: AppCompatActivity(){
         } catch(e:Exception) {
             Log.i("Print", "Could not find authSignin in preferences ${e.stackTrace}")
         }
+
     }
 
     private fun setupNavigation() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true) //Adds back arrow to close open menu
+        if (user != null && user?.ingame_name != null) {
+            navigationUserName.text = user?.ingame_name
+        } else if (user != null) {
+            navigationUserName.text = "Please verify account at warframe.market"
+        } else {
+            navigationUserName.text = "Not signed in."
+        }
+
+
         navigationView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.navigation_buySell -> Log.i(TAG, "Clicked on buy Sell")
+                R.id.navigation_buySell -> {
+                    Toast.makeText(applicationContext, "Clicked on Buy Sell", Toast.LENGTH_SHORT).show()
+                    Log.i(TAG, "Clicked on buy sell!")
+                }
                 R.id.navigation_items -> Toast.makeText(applicationContext, "Clicked on Items", Toast.LENGTH_SHORT).show()
                 R.id.navigation_contracts -> Toast.makeText(applicationContext, "Clicked on Contracts", Toast.LENGTH_SHORT).show()
                 R.id.navigation_lich -> Toast.makeText(applicationContext, "Clicked on Lich", Toast.LENGTH_SHORT).show()
